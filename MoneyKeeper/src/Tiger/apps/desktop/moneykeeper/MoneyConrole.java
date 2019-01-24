b43f73861e1c42ba5ada127dd4ad020a9df78fc9
RayMiles94 <natsuboy94@gmail.com>
2019-01-24 16:07:48 +0100

    add connection between application and server

--- MoneyKeeper/src/Tiger/apps/desktop/moneykeeper/MoneyConrole.java
+++ MoneyKeeper/src/Tiger/apps/desktop/moneykeeper/MoneyConrole.java
@@ -3,8 +3,12 @@ package Tiger.apps.desktop.moneykeeper;
 public class MoneyConrole {
 	MoneyView view;
 	MoneyModel model;
+	
+	
 	public MoneyConrole(MoneyModel m,MoneyView v){
 		this.view     = v;
 		this.model = m;
 	}
+	
+	
 }
--- MoneyKeeper/src/Tiger/apps/desktop/moneykeeper/MoneyModel.java
+++ MoneyKeeper/src/Tiger/apps/desktop/moneykeeper/MoneyModel.java
@@ -8,13 +8,17 @@ public class MoneyModel {
 	String client;
 	String mp;
 	int money;
+	Connection conn;
+	Statement mystat;
 	
-	public MoneyModel(){
+	public MoneyModel() throws SQLException{
 	   card = 0;
 	   codecard=0;
 	   client="";
 	   mp="";
 	   money=0;
+	   conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/moneykeeper","root","");
+	   mystat = conn.createStatement();
    }
 	
 }
