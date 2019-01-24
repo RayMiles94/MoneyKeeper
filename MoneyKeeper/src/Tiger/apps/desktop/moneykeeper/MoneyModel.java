9d7a3441919440ac78d142b1b7095e3bec62313d
RayMiles94 <natsuboy94@gmail.com>
2019-01-24 15:57:45 +0100

    add membres to model constrcuteur

--- MoneyKeeper/src/Tiger/apps/desktop/moneykeeper/MoneyModel.java
+++ MoneyKeeper/src/Tiger/apps/desktop/moneykeeper/MoneyModel.java
@@ -1,5 +1,16 @@
 package Tiger.apps.desktop.moneykeeper;
 
 public class MoneyModel {
-   public MoneyModel(){}
+	int card;
+	int codecard;
+	String client;
+	String mp;
+	int money;
+   public MoneyModel(){
+	   card = 0;
+	   codecard=0;
+	   client="";
+	   mp="";
+	   money=0;
+   }
 }
