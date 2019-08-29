67843825e604c7f005b323f3d9cd793b16d6da3a
RayMiles <32762853+RayMiles94@users.noreply.github.com>
2019-08-29 11:20:05 +0200

    [FIX] ugly code

--- MoneyKeeper/src/Tiger/apps/desktop/moneykeeper/MoneykeeperMain.java
+++ MoneyKeeper/src/Tiger/apps/desktop/moneykeeper/MoneykeeperMain.java
@@ -4,15 +4,12 @@ package Tiger.apps.desktop.moneykeeper;
 import java.sql.SQLException;
 
 public class MoneykeeperMain {
-
 	public static void main(String[] args) throws SQLException {
-	  MoneyModel model = new MoneyModel();
-	  MoneyView view = new MoneyView();
-	  @SuppressWarnings("unused")
-	  MoneyConrole control = new MoneyConrole(model,view);
-      view.setVisible(true);
+	  	MoneyModel model = new MoneyModel();
+	 	MoneyView view = new MoneyView();
+		
+	  	@SuppressWarnings("unused")
+	  	MoneyConrole control = new MoneyConrole(model,view);
+     	  	view.setVisible(true);
 	}
-
-	
-
 }
